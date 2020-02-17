16a192b09c2d28a99ed56f213c476e603e18ea52
Gustavo Cotta <gmcotta34@gmail.com>
2020-02-17 11:52:04 -0300

    Get and Display Posts

--- src/app.js
+++ src/app.js
@@ -1,2 +1,10 @@
-const greeting = 'Hello world';
-console.log(greeting);
+import { http } from './http';
+import { ui } from './ui';
+
+document.addEventListener('DOMContentLoaded', getPosts);
+
+function getPosts() {
+  http.get('http://localhost:3000/posts')
+    .then(data => ui.showPosts(data))
+    .catch(err => console.log(err));
+}
